01acdcd68bcf98ffde59a75365e9ea4135978f6e
madhan-swe <madhankumarcvkl@gmail.com>
2020-12-02 21:16:53 +0530

    Added login, registration, ChangePassword

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,13 @@ function checkNull(element, elementName)
     }
     return true
 }
+
+function goHome()
+{
+    window.location.href = FRONT_END_URL;
+}
+
+
 function login()
 {
     let email = document.getElementById("email").value;
@@ -46,7 +53,7 @@ function login()
                 document.getElementById("alertType").style.display = "block";
             }
         })
-        return false
+        return true
     }
     return false
 }
@@ -57,9 +64,186 @@ function register()
     let password = document.getElementById("password").value;
     let firstName = document.getElementById("firstname").value;
     let lastName = document.getElementById("lastname").value;
-    if(checkNull(email, "Email") && checkNull(password, "Password") &&
-       checkNull(firstName, "First Name") && checkNull(lastName, "Last Name"))
+    if(checkNull(email, "Email") && checkNull(firstName, "First Name") &&
+    checkNull(password, "Password") && checkNull(lastName, "Last Name"))
     {
-        alert("Success !")
+        let data = {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: password
+        }
+        fetch(BACKEND_URL + "/register", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }).then((res)=>res.json())
+        .then((res)=> {
+            console.log(res)
+            if(!res.result)
+            {
+                document.getElementById("alertType").className = '';
+                document.getElementById("alertType").classList.add("alert", "alert-danger", "alert-dismissible");
+                document.getElementById("alertKey").innerText = res.status;
+                document.getElementById("alertMessage").innerText =  res.message;
+                document.getElementById("alertType").style.display = "block";
+                return 
+            }
+            document.getElementById("alertType").className = '';
+            document.getElementById("alertType").classList.add("alert", "alert-success", "alert-dismissible");
+            document.getElementById("alertKey").innerText = res.status;
+            document.getElementById("alertMessage").innerText =  res.message;
+            document.getElementById("alertType").style.display = "block";
+        })
+    }
+}
+
+function activateAccount()
+{
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get("token");
+    fetch(BACKEND_URL + "/users/active/" + token, {
+        method: "POST"
+    }).then((res)=>res.json())
+    .then((res)=> {
+        if(!res.result)
+        {
+            document.getElementById("errorTitle").innerText = "Activation Failed";
+            document.getElementById("errorText").innerText = res.message;
+            $("#errorModalLong").modal("show");
+            
+            return
+        }
+        document.getElementById("errorTitle").innerText = "Activation Successful";
+        document.getElementById("errorText").innerText = res.message;
+        $("#errorModalLong").modal("show");
+        return
+        
+    });
+}
+
+function confirmEmail()
+{
+    let email = document.getElementById("email").value;
+    if(checkNull(email, "Email"))
+    {
+        fetch(BACKEND_URL + "/users/" + email, {
+            method: "POST"
+        }).then((res)=>res.json())
+        .then((res)=> {
+            console.log(res)
+            if(!res.result)
+            {
+                document.getElementById("alertType").className = '';
+                document.getElementById("alertType").classList.add("alert", "alert-danger");
+                document.getElementById("alertKey").innerText = res.status;
+                document.getElementById("alertMessage").innerText =  res.message;
+                document.getElementById("alertType").style.display = "block";
+                return 
+            }
+        })
+
+        fetch(BACKEND_URL + "/users/forgotPassword/" + email, {
+            method: "POST"
+        }).then((res)=>res.json())
+        .then((res)=> {
+            console.log(res)
+            if(!res.result)
+            {
+                document.getElementById("alertType").className = '';
+                document.getElementById("alertType").classList.add("alert", "alert-danger");
+                document.getElementById("alertKey").innerText = res.status;
+                document.getElementById("alertMessage").innerText =  res.message;
+                document.getElementById("alertType").style.display = "block";
+                return 
+            }
+            document.getElementById("alertType").className = '';
+            document.getElementById("alertType").classList.add("alert", "alert-success");
+            document.getElementById("alertKey").innerText = res.status;
+            document.getElementById("alertMessage").innerText =  res.message;
+            document.getElementById("alertType").style.display = "block";
+        })
+    }
+}
+
+function verifyPasswordResetLink()
+{
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get("token");
+    const email = urlParams.get("email");
+    
+    fetch(BACKEND_URL + "/users/passwordReset/" + email, {
+        method: "POST",
+        body: JSON.stringify({
+            email
+        }),
+        "Content-type": "application/json"
+    }).then((res)=>res.json())
+    .then((res)=> {
+        if(!res.result)
+        {
+            document.getElementById("errorTitle").innerText = "Password Reset Failed";
+            document.getElementById("errorText").innerText = res.message;
+            $("#errorModalLong").modal("show");
+            
+            return
+        }
+        window.location.href = "resetPassword.html?email=" + email;
+        
+    });
+}
+
+function updatePassword()
+{
+    let newPassword = document.getElementById("password").value;
+    let CPassword = document.getElementById("cpassword").value;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const email = urlParams.get("email");
+
+    if(checkNull(newPassword, "New Password") && checkNull(CPassword, "Confirm Password"))
+    {
+        if(newPassword!==CPassword)
+        {
+            document.getElementById("alertType").className = '';
+            document.getElementById("alertType").classList.add("alert", "alert-danger");
+            document.getElementById("alertKey").innerText = "Validation Error: ";
+            document.getElementById("alertMessage").innerText =  "Passwords Don't match !";
+            document.getElementById("alertType").style.display = "block";
+            return
+        }
+      
+        fetch(BACKEND_URL + "/users/changePassword/" + email, {
+            method: "POST",
+            body: JSON.stringify({
+                password: CPassword 
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }).then((res)=>res.json())
+        .then((res)=> {
+            console.log(res)
+            if(!res.result)
+            {
+                document.getElementById("alertType").className = '';
+                document.getElementById("alertType").classList.add("alert", "alert-danger");
+                document.getElementById("alertKey").innerText = res.status;
+                document.getElementById("alertMessage").innerText =  res.message;
+                document.getElementById("alertType").style.display = "block";
+                return 
+            }
+            document.getElementById("alertType").className = '';
+            document.getElementById("alertType").classList.add("alert", "alert-success");
+            document.getElementById("alertKey").innerText = res.status;
+            document.getElementById("alertMessage").innerText =  res.message;
+            document.getElementById("alertType").style.display = "block";
+            setTimeout(()=>{ window.location.href = FRONT_END_URL}, 1300)
+        })
+
     }
 }
